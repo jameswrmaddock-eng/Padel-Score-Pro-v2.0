79bf5befeba5d3c8f8e76fa0a3ac04bc22a8b051
James Maddock <jamesmaddock@macbookair.lan>
2026-04-11 14:43:13 +0100

    feat: add RecommendedGear section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/layout/Header';
 import Hero from '@/components/sections/Hero';
 import RecentMatches from '@/components/sections/RecentMatches';
+import RecommendedGear from '@/components/gear/RecommendedGear';
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
       <Hero />
       <div className="h-px bg-white/[0.06] mx-10" />
       <RecentMatches />
+      <div className="h-px bg-white/[0.06] mx-10" />
+      <RecommendedGear />
     </main>
   );
 }
